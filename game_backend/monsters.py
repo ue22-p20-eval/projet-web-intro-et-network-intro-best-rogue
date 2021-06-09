e252cb09300990dfc73183a9b2c9b0a8ed5bcb6c
VIGNON <colin.vignon@mines-paristech.fr>
2021-06-09 23:28:03 +0200

    2eme changement

--- game_backend/monsters.py
+++ game_backend/monsters.py
@@ -31,10 +31,11 @@ class Monster:
         new_x = self._x + dx
         new_y = self._y + dy
 
-        if map[new_y][new_x] == ".":
+        if map[new_y][new_x] == "." or map[new_y][new_x] == "x" :
             ret =True
             map[new_y][new_x] = self._symbol
-            data = [{"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+            map[self._y][self._x] = "."
+            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"."}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
             self._x = new_x
             self._y = new_y
         else:

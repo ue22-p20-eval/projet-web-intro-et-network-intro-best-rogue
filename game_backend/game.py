839995b9bcbda15a0184cef9b63f83b602a39c60
Chloé Vogel <chloe.vogel@mines-paristech.fr>
2021-06-11 23:23:41 +0200

    voilà

--- game_backend/game.py
+++ game_backend/game.py
@@ -14,8 +14,7 @@ class Game:
         self._player = Player()
         self._player.initPos( self._map )
 
-        self._monster = Monster()
-        self._monster.initPosM( self._map)
+        #self._Monster = self._generator.gen_monster(self)
 
     def getMap(self):
         return self._map
@@ -23,5 +22,12 @@ class Game:
     def move(self, dx, dy):
         return self._player.move(dx, dy, self._map)
 
+    def update_Monster(self):
+        data_list = []
+        for monster in self._Monster:
+            data = monster.moveM(self._map)
+            data_list.append(data)
+        return data_list
+        
     def moveM(self, dx = rd.randint(-1,1), dy = rd.randint(-1,1)):
         return self._monster.moveM(dx, dy, self._map)

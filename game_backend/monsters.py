88fa1d70ef1f65aabe50e96b915fc2a76d2b4b62
VIGNON <colin.vignon@mines-paristech.fr>
2021-06-11 22:52:10 +0200

    monstre

--- game_backend/monsters.py
+++ game_backend/monsters.py
@@ -5,27 +5,27 @@ class Monster:
         self._symbol = symbol
         self._x = None
         self._y = None
+        self._dx = None
+        self._dy = None
 
-    def initPosM(self, _map):
-        n_row = len(_map)
-        #n_col = len(_map[0])
-
-        y_init = rd.randint(0, n_row//2)
-        found = False
-        while found is False:
-            y_init += 1
-            for i,c in enumerate(_map[y_init]):
-                if c == ".":
-                    x_init = i
-                    found = True
-                    break
+    def initPos(self, _map, height, width):
+        y_init = random.randint(0, height-1)  
+        x_init = random.randint(0, width-1)
 
         self._x = x_init
         self._y = y_init
 
         _map[self._y][self._x] = self._symbol
 
-    def moveM(self, dx, dy, map):
+
+    def dxdy(self):
+        dx, dy = 0, 0
+        dx,dy = rd.choice([[0, 0], [0, 1], [0, 0], [0, -1], [0, 0], [1, 0], [0, 0], [-1, 0], [0, 0]])
+    
+        return dx,dy
+
+    def moveM(self, map):
+        dx, dy = self.dxdy()
         new_x = self._x + dx
         new_y = self._y + dy
 
@@ -33,9 +33,11 @@ class Monster:
             ret =True
             map[new_y][new_x] = self._symbol
             map[self._y][self._x] = "."
-            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"."}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":self.previous_step_on}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol},[dy,dx]]
             self._x = new_x
             self._y = new_y
+            self._dx = dx
+            self._dy = dy
         else:
             ret = False
             data = []

4793cf1b13cd5940fb103fd0f13e40f195f35451
Chloé Vogel <chloe.vogel@mines-paristech.fr>
2021-06-11 22:39:28 +0200

    on repart sur des bonnes bases

--- game_backend/player.py
+++ game_backend/player.py
@@ -40,13 +40,27 @@ class Player:
         new_x = self._x + dx
         new_y = self._y + dy
 
-        if map[new_y][new_x] == "." or map[new_y][new_x] == "x" :
+        if map[new_y][new_x] == "." :
             ret =True
             map[new_y][new_x] = self._symbol
             map[self._y][self._x] = "."
             data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"."}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
             self._x = new_x
             self._y = new_y
+        elif map[new_y][new_x] == "$" :
+            ret = True
+            map[new_y][new_x] = self._symbol
+            map[self._y][self._x] = "."
+            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"."}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+            self._x = new_x
+            self._y = new_y
+            self._money += 1
+        elif map[new_y][new_x] == "M" :
+            ret = True
+            map[new_y][new_x] = self._symbol
+            map[self._y][self._x] = "."
+            data = [{"i": f"{self._y}", "j":f"{self._x}", "content":"."}, {"i": f"{new_y}", "j":f"{new_x}", "content":self._symbol}]
+            self._life -= 1
         else:
             ret = False
             data = []
